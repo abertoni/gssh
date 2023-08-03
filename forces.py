a3e06363453c454d3d6b8f6475d7ebf5cd38d19d
abertoni <andresibertoni@gmail.com>
2023-08-03 15:45:47 -0300

    Now the steepest descent implementation relies on forces (also used in dynamics). All implemented but not tested.

--- forces.py
+++ forces.py
@@ -2,40 +2,87 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from .lattice_tools import get_neighbour_matrix
 from .electronic import get_neighbour_sites_projection
+from .sparse_tools import sp_roll
 
-def compute_forces(phaselinks, state_vectors, parameters):
+def compute_lattice_shift(positions, forces, parameters):
     raise NotImplementedError()
-    #return forces
+    #return positions_shift
 
-def compute_lattice_shift(phaselinks, forces, parameters):
-    raise NotImplementedError()
-    #return phaselinks_shift
-
-def steepest_descent_step(phaselinks, state_vectors, occupations, parameters):
-    # Derived analytically using the Hellmann–Feynman theorem
-    # (only first order hopping is implemented)
-    hopping_params = parameters["hopping_parameters"]
-    if len(hopping_params) > 2: raise NotImplementedError()
-    A = hopping_params[1] # first-order coupling
+def get_lattice_matrix(neighbours, parameters):
     # (only harmonic oscillation is implemented)
     oscillator_params = parameters["oscillator_parameters"]
     if len(oscillator_params) > 1: raise NotImplementedError()
     K = oscillator_params[0] # harmonic constant
-    # Compute new phaselinks
-    new_phaselinks = np.zeros(phaselinks.size)
-    for jdx,spin in enumerate(["up","down"]):
-        states_dot_product = get_neighbour_sites_projection(n_shift, states_vectors, occupations[:,jdx], parameters)
-        new_phaselinks += - A/K * states_dot_product
-    # Apply correction for periodic boundary conditions
+	# Lattice matrix is partially used to compute lattice forces
+    neighbours = [-1, +1]
+    P = get_paired_matrix(neighbours, parameters)
+    lattice_matrix = - K * P
+    return lattice_matrix
+
+def get_lattice_forces(positions, velocities, parrameters):
+    """Forces from classical potential for inter-nuclear interactions."""
+    # (only harmonic oscillation is implemented)
+    oscillator_params = parameters["oscillator_parameters"]
+    if len(oscillator_params) > 1: raise NotImplementedError()
+    K = oscillator_params[0] # harmonic constant
+    # Compute forces from other nuclei
+    neighbours = [-1, +1]
+	sum_rel_positions = get_sum_relative_positions(positions, neighbours, parameters)
+	lattice_forces = - K * sum_rel_positions
+    return lattice_forces
+
+def open_boundary_forces(parameters):
+    n_sites = parameters["number_of_sites"]
     is_periodic = parameters["periodic_boundaries"]
-    if is_periodic: new_phaselinks -= np.mean(new_phaselinks)
-    elif "open_boundary_stretching" in parameters.keys():
+    # Open boundary stretching
+    open_boundary_forces = np.zeros(n_sites)
+    if not is_periodic and "open_boundary_stretching" in parameters.keys():
         G = parameters["open_boundary_stretching"]
-        new_phaselinks += G/K
-    # Compute the shift in phaselinks
-    phaselinks_shift = new_phaselinks - phaselinks
-    return phaselinks_shift
+		open_boundary_forces[0] = -G
+		open_boundary_forces[-1] = +G
+	return open_boundary_forces
+
+def get_electronic_forces(state_vectors, occupations, parameters):
+    """Mean field forces from electrons."""
+    # (only first order hopping is implemented)
+    hopping_params = parameters["hopping_parameters"]
+    if len(hopping_params) > 2: raise NotImplementedError()
+    A = hopping_params[1] # first-order coupling
+    # Compute forces from electrons
+    electronic_forces = np.zeros(n_sites)
+    for jdx,spin in enumerate(["up","down"]):
+        states_dotprod_left = get_neighbour_sites_projection(states_vectors, occupations[:,jdx], -1, parameters)
+        states_dotprod_right = get_neighbour_sites_projection(states_vectors, occupations[:,jdx], +1, parameters)
+        if is_periodic:
+            states_dotprod_left[0] = 0
+            states_dotprod_right[-1] = 0
+        electronic_forces += - A * (states_dotprod_left - states_dotprod_right)
+    return electronic_forces
+
+def compute_forces(positions, velocities, state_vectors, occupations, parameters):
+    forces = lattice_forces(positions, velocities, parameters)
+    forces += open_boundary_forces(parameters)
+    forces += electronic_forces(state_vectors, occupations, parameters)
+    return forces
+
+def steepest_descent_step(positions, state_vectors, occupations, parameters):
+    n_sites = parameters["number_of_sites"]
+    is_periodic = parameters["periodic_boundaries"]
+    # Derived analytically using the Hellmann–Feynman theorem
+    # Lattice matrix
+    neighbours = [-1, +1]
+    lattice_matrix = get_lattice_matrix(neighbours, parameters)
+    # Electronic forces
+    electronic_forces = get_electronic_forces(state_vectors, occupations, parameters)
+    # Open boundary correction forces
+    open_boundary_forces = open_boundary_forces(parameters)
+    # New positions computed by solving system of linear equations
+    new_positions = np.linalg.solve(-lattice_matrix, electronic_forces + open_boundary_forces)
+    # Compute the shift in positions
+    positions_shift = new_positions - positions
+    return positions_shift
 
 ###############################################################
 #       Leandro Manuel Arancibia & Andrés Ignacio Bertoni     #
