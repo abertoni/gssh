7cc8da2773d0421a8f25b489596d434bf6c13e45
abertoni <andresibertoni@gmail.com>
2023-08-03 01:00:59 -0300

    Added a more general way to compute sum of relative positions of neighbours with respect to site; this makes available the neighbour matrix (useful to other functions).

--- lattice_tools.py
+++ lattice_tools.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from gssh.sparse_tools import sp_roll
 
 def initialize_positions(parameters):
     n_sites = parameters["number_of_sites"]
@@ -13,15 +14,29 @@ def initialize_positions(parameters):
         positions += (noise/100) * np.random.uniform(-1, 1, n_sites)
     return positions
 
-def get_neighbour_distances(positions, shift, parameters):
+def get_neighbour_matrix(neighbours, parameters):
     n_sites = parameters["number_of_sites"]
+    a = parameters["lattice_parameter"]
     is_periodic = parameters["periodic_boundaries"]
-    dr_n = np.roll(r_n, -(shift)) - r_n
+    N = np.zeros((n_sites, n_sites))
+    for nidx in neighbours:
+        n = np.ones(n_sites)
+        if not is_periodic: n[::np.sign(nidx)][-abs(nidx):] = 0
+        N += -sp_roll(np.diag(n), -nidx).toarray()
+    N += -np.diag(np.sum(N, axis=-1))
+    return N
+
+def get_sum_relative_positions(positions, neighbours, parameters):
+    n_sites = parameters["number_of_sites"]
+    a = parameters["lattice_parameter"]
+    is_periodic = parameters["periodic_boundaries"]
+    N = get_neighbour_matrix(neighbours, parameters)
+    sum_rel_positions = -N @ positions
     if is_periodic:
-        a = parameters["lattice_parameter"]
-        dr_n[-1] += n_sites * a
-    else: dr_n = dr_n[:-1]
-    return dr_n
+        for neig_idx in neighbours:
+            correction = np.sign(neig_idx) * a * n_sites
+            sum_rel_positions[::np.sign(neig_idx)][-abs(neig_idx):] += correction
+    return sum_rel_positions
 
 def check_lattice_minimum(phaselinks_shift, parameters):
     tolerance = parameters["lattice_optimization_tolerance"]
