b3022704632939369608ecee448b536936000216
abertoni <andresibertoni@gmail.com>
2023-08-03 00:59:16 -0300

    Conversion to positions plus simplification of neighbour sites projection function in the electronic module.

--- electronic.py
+++ electronic.py
@@ -3,27 +3,25 @@
 
 from .hamiltonian import build_hamiltonian, diagonalize_hamiltonian
 
-def electronic_minimization(phaselinks, parameters):
+def electronic_minimization(positions, parameters):
     raise NotImplementedError()
     #return state_energies, state_vectors
 
-def get_electronic_solutions(phaselinks, parameters):
+def get_electronic_solutions(positions, parameters):
     lat_opt_mode = parameters["lattice_optimization_mode"].lower()
     if "numerical" in lat_opt_mode:
-        state_energies, state_vectors = electronic_minimization(phaselinks, parameters)
+        state_energies, state_vectors = electronic_minimization(positions, parameters)
     elif "analytical" in lat_opt_mode:
-        hamiltonian = build_hamiltonian(phaselinks, parameters)
+        hamiltonian = build_hamiltonian(positions, parameters)
         state_energies, state_vectors = diagonalize_hamiltonian(hamiltonian, parameters)
     return state_energies, state_vectors
 
-def get_neighbour_sites_projection(n_shift, states_vectors, occupations, parameters):
-    """Projection between n+n_shift and n+n_shift+1."""
+def get_neighbour_sites_projection(states_vectors, occupations, shift, parameters):
     n_sites = parameters["number_of_sites"]
     is_periodic = parameters["periodic_boundaries"]
-    phaselinks_size = n_sites - (0 if is_periodic else 1)
-    states_dot_product = np.sum(occupations * np.conj(np.roll(state_vectors, -(n_shift), axis=0)) * np.roll(state_vectors, -(n_shift+1), axis=0), axis=1)
+    states_dot_product = np.sum(occupations * np.conj(state_vectors) * np.roll(state_vectors, -shift, axis=0), axis=1)
     if np.linalg.norm(np.imag(states_dot_product)) > 0: print("Warning! Imaginary part of neighbor sites projection is non-zero.")
-    states_dot_product = 2 * np.real(states_dot_product)[:phaselinks_size] # h.c.
+    states_dot_product = 2 * np.real(states_dot_product) # h.c.
     return states_dot_product
 
 ###############################################################
