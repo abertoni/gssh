2551db80011d0a291d75ddeec09db88982290380
abertoni <andresibertoni@gmail.com>
2023-08-03 00:56:20 -0300

    Unification of modules for lattice.

--- lattice_optimization.py
+++ lattice_optimization.py
@@ -1,33 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from .forces import compute_forces, compute_lattice_shift, steepest_descent_step
 from .electronic import get_electronic_solutions
-from .lattice import update_lattice
-from .lattice_tools import positions_to_phaselinks, phaselinks_to_positions, check_lattice_minimum
+from .lattice_tools import get_neighbour_matrix, check_lattice_minimum
 from .occupations import compute_occupations
 
+def update_lattice(positions, state_vectors, occupations, parameters):
+    lat_opt_mode = parameters["lattice_optimization_mode"].lower()
+    # Via numerical force computation
+    if "numerical" in lat_opt_mode:
+        forces = compute_forces(positions, state_vectors, occupations, parameters)
+        positions_shift = compute_lattice_shift(positions, forces, parameters)
+    # Via Helmann-Feynman analytic solution
+    elif "analytical" in lat_opt_mode:
+        positions_shift = steepest_descent_step(positions, state_vectors, occupations, parameters)
+    return positions_shift
+
 def lattice_optimization(positions, parameters):
-    # Converts to phaselinks
-    phaselinks = positions_to_phaselinks(positions, parameters)
     # Optimization is a recursive procedure
-    max_steps = parameters["maximum_lattice_optimization_steps"] ; is_optimized = False
+    max_steps = parameters["maximum_lattice_optimization_steps"]
+    is_optimized = False
     for idx in range(max_steps):
         # Compute shift of lattice towards minimum
-        state_energies, state_vectors = get_electronic_solutions(phaselinks, parameters)
+        state_energies, state_vectors = get_electronic_solutions(positions, parameters)
         occupations = compute_occupations(state_energies, parameters)
-        phaselinks_shift = update_lattice(phaselinks, state_vectors, occupations, parameters)
-        # Updates the lattice
-        phaselinks += phaselinks_shift
+        positions_shift = update_lattice(positions, state_vectors, occupations, parameters)
         # Checks end criteria
-        is_optimized = check_lattice_minimum(phaselinks_shift, parameters)
+        is_optimized = check_lattice_minimum(positions_shift, parameters)
         if is_optimized: break
+        else: positions += positions_shift
     # Check if optimization is done
     if is_optimized: print(f"Optimization finished after {idx+1}!")
     else: print(f"The lattice is not yet optimized after {max_steps}!")
-    # Restore positions
-    optimized_positions = phaselinks_to_positions(phaselinks, parameters)
     # Returns positions and electronic states (+ energies)
-    return optimized_positions, state_energies, state_vectors
+    return positions, state_energies, state_vectors
 
 ###############################################################
 #       Leandro Manuel Arancibia & Andrés Ignacio Bertoni     #
