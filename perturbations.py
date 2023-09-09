a331b57e1de3637539eb1a09bdbe356ef525098f
abertoni <andresibertoni@gmail.com>
2023-09-08 21:22:33 -0300

    Onsite energies can be provided as a single array for the number of sites, many of them for a time series, or as a tuple of three parameters (voltage, gate width and gate separation, the first in eV and the latter two as number of sites or as a proportion from 0 to 1) which will be used to construct a single array of onsite energies corresponding to opposing voltage gates.

--- perturbations.py
+++ perturbations.py
@@ -9,13 +9,6 @@ def get_vector_potential(time, parameters):
     vector_potential = np.interp(time, t_grid, A_t)
     return vector_potential
 
-def get_onsite_energies(time, parameters):
-    try:
-        t_grid, onsites_t = parameters["onsite_perturbation"]
-        onsite_energies = np.interp(time, t_grid, onsites_t)
-    except: onsite_energies = parameters["onsite_perturbation"]
-    return onsite_energies
-
 def gen_onsite_gates(potential_eV, width, separation, parameters, plus_minus=True):
     eV_to_Eh = 1/spc.physical_constants["Hartree energy in eV"][0] # Eh/eV
     n_sites = parameters["number_of_sites"]
@@ -32,6 +25,21 @@ def gen_onsite_gates(potential_eV, width, separation, parameters, plus_minus=Tru
     onsite_gates[(side):(side+width)] = l_sign * potential_eV * eV_to_Eh
     onsite_gates[::-1][(side):(side+width)] = r_sign * potential_eV * eV_to_Eh
     return onsite_gates
+
+def get_onsite_energies(time, parameters):
+    n_params = len(parameters["onsite_perturbation"])
+    if n_params == parameters["number_of_sites"]:
+        onsite_energies = parameters["onsite_perturbation"]
+    elif n_params == 3:
+        potential_eV, width, separation = parameters["onsite_perturbation"]
+        if "onsite_plus_minus" not in parameters: plus_minus = True
+        else: plus_minus = parameters["onsite_plus_minus"]
+        onsite_energies = gen_onsite_gates(potential_eV, width, separation, parameters, plus_minus)
+    elif n_params == 2:
+        t_grid, onsites_t = parameters["onsite_perturbation"]
+        onsite_energies = np.interp(time, t_grid, onsites_t)
+    else: raise RuntimeError("Provided format for onsite_perturbation is not allowed!")
+    return onsite_energies
 
 ###############################################################
 #       Leandro Manuel Arancibia & Andrés Ignacio Bertoni     #
